beefb9988773c514c889b3167b504ef32180db6a
Alexander Veselov <Alexander_Veselov@yahoo.com>
2019-08-08 11:21:07 +0300

    Submit an empty cmd buffer

--- main.cpp
+++ main.cpp
@@ -42,6 +42,11 @@ void RunApplication()
     std::vector<char*> device_layer_names;
     std::vector<char*> device_extension_names;
 
+    std::vector<VkCommandPool> command_pools;
+    command_pools.resize(physical_devices.size());
+    std::vector<VkCommandBuffer> command_buffers;
+    command_buffers.resize(physical_devices.size());
+
     for (auto i = 0u; i < physical_devices.size(); ++i)
     {
         VkDeviceQueueCreateInfo queue_create_info = {};
@@ -64,7 +69,63 @@ void RunApplication()
 
         result = vkCreateDevice(physical_devices[i], &device_create_info, nullptr, &vk_devices[i]);
         CHECK_RESULT();
+
+        VkCommandPoolCreateInfo command_pool_create_info = {};
+        command_pool_create_info.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+        command_pool_create_info.queueFamilyIndex = 0u;
+
+        result = vkCreateCommandPool(vk_devices[i], &command_pool_create_info, nullptr, &command_pools[i]);
+        CHECK_RESULT();
+
+        VkCommandBufferAllocateInfo cmd_buffer_allocate_info = {};
+        cmd_buffer_allocate_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        cmd_buffer_allocate_info.commandPool = command_pools[i];
+        cmd_buffer_allocate_info.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+        cmd_buffer_allocate_info.commandBufferCount = 1u;
+        result = vkAllocateCommandBuffers(vk_devices[i], &cmd_buffer_allocate_info, &command_buffers[i]);
+        CHECK_RESULT();
+
+        VkCommandBufferBeginInfo cmd_buffer_begin_info = {};
+        cmd_buffer_begin_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+        result = vkBeginCommandBuffer(command_buffers[i], &cmd_buffer_begin_info);
+        CHECK_RESULT();
+
+        result = vkEndCommandBuffer(command_buffers[i]);
+        CHECK_RESULT();
+
+        VkQueue queue;
+        vkGetDeviceQueue(vk_devices[i], 0u, 0u, &queue);
+
+        VkSubmitInfo submit_info = {};
+        submit_info.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+        submit_info.waitSemaphoreCount = 0u;
+        submit_info.pWaitSemaphores = nullptr;
+        submit_info.pWaitDstStageMask = nullptr;
+        submit_info.commandBufferCount = 1u;
+        submit_info.pCommandBuffers = &command_buffers[i];
+        submit_info.signalSemaphoreCount = 0u;
+        submit_info.pSignalSemaphores = nullptr;
+        result = vkQueueSubmit(queue, 1u, &submit_info, nullptr);
+        CHECK_RESULT();
     }
+
+    //VkPipelineShaderStageCreateInfo shader_stage_create_info = {};
+    //shader_stage_create_info.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    //shader_stage_create_info.flags;
+    //shader_stage_create_info.stage;
+    //shader_stage_create_info.module;
+    //shader_stage_create_info.pName;
+    //shader_stage_create_info.pSpecializationInfo;
+
+    //VkComputePipelineCreateInfo pipeline_create_info = {};
+    //pipeline_create_info.flags;
+    //pipeline_create_info.stage;
+    //pipeline_create_info.layout;
+    //pipeline_create_info.basePipelineHandle
+    //pipeline_create_info.basePipelineIndex;
+    //pipeline_create_info.sType = VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO;
+
+    //vkCreateComputePipelines()
 
 }
 
